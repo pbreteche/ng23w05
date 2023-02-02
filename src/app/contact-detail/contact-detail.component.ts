fe625709c88fbd779bdf7e2c4c9b8fc8d832c1d0
Pierre Bretéché <pbreteche@dawan.fr>
2023-02-02 16:45:25 +0100

    observable dans le composant et template

--- src/app/contact-detail/contact-detail.component.ts
+++ src/app/contact-detail/contact-detail.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {Contact} from "../../model/contact";
 import {CurrentContactService} from "../contact/current-contact.service";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-contact-detail',
@@ -8,11 +9,9 @@ import {CurrentContactService} from "../contact/current-contact.service";
   styleUrls: ['./contact-detail.component.scss']
 })
 export class ContactDetailComponent {
-  contact?: Contact;
+  contact$: Observable<Contact>;
 
-  constructor(private currentContact: CurrentContactService) {
-    currentContact.getData().subscribe(
-      (contact: Contact) => this.contact = contact
-    );
+  constructor(currentContact: CurrentContactService) {
+    this.contact$ = currentContact.getData();
   }
 }
